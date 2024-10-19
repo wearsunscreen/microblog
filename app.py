800767724012741a5b1743ef4416fa37e3f9e21a
John Crane <john.crane@gmail.com>
2024-10-19 14:24:37 -0700

    passing entry from form to template

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import datetime
 
 app = Flask(__name__)
 
+entries = []
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         entry_content = request.form.get('content')
         formatted_date = datetime.datetime.today().strftime('%Y-%m-%d')
-        print(entry_content, formatted_date)
+        entries.append((entry_content, formatted_date))
 
-    return render_template('home.html')
+    return render_template('home.html', entries=entries)
