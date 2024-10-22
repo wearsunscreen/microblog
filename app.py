0b145e880235457a021615066796215ed09c952b
John Crane <john.crane@gmail.com>
2024-10-22 09:41:37 -0700

    Saving to Mongo

--- app.py
+++ app.py
@@ -16,5 +16,14 @@ def home():
         entry_content = request.form.get('content')
         formatted_date = datetime.datetime.today().strftime('%Y-%m-%d')
         entries.append((entry_content, formatted_date))
+        app.db.entries.insert_one({"content": entry_content, "data": formatted_date})
 
-    return render_template('home.html', entries=entries)
+    entries_with_date = [
+        (
+            entry[0], 
+            entry[1],
+            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime('%b %d')
+        )
+        for entry in entries
+    ]
+    return render_template('home.html', entries=entries_with_date)
