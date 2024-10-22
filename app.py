3a11bbbae23ba2bed1489745fbb5a01710c26dbb
John Crane <john.crane@gmail.com>
2024-10-22 09:58:55 -0700

    Storing entries to mongo

--- app.py
+++ app.py
@@ -6,24 +6,19 @@ app = Flask(__name__)
 client = MongoClient("mongodb+srv://wearsunscreen:Brillig55@cluster0.ivgo2.mongodb.net/")
 app.db = client.microblog
 
-entries = []
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    print([e for e in app.db.entries.find({})])
-
     if request.method == 'POST':
         entry_content = request.form.get('content')
         formatted_date = datetime.datetime.today().strftime('%Y-%m-%d')
-        entries.append((entry_content, formatted_date))
-        app.db.entries.insert_one({"content": entry_content, "data": formatted_date})
+        app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
 
     entries_with_date = [
         (
-            entry[0], 
-            entry[1],
-            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime('%b %d')
+            entry["content"], 
+            entry["date"],
+            datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime('%b %d')
         )
-        for entry in entries
+        for entry in app.db.entries.find({})
     ]
     return render_template('home.html', entries=entries_with_date)
