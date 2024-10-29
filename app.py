2d56ebfcc4777776963f3a1500a9ba4435cc9367
John Crane <john.crane@gmail.com>
2024-10-29 07:57:51 -0700

    adding secret key for sessions, cookies etc.

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ load_dotenv()
 # Flask app factory, must be named 'create_app'.
 def create_app():
     app = Flask(__name__)
+    app.secret_key = os.getenv("SECRET_KEY")
     app.register_blueprint(pages)
     client = MongoClient(os.getenv("MONGODB_URI"))
     app.db = client.get_default_database(default="microblog")
